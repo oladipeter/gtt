dfd10d250142b3915e22161c7bbded7628828713
oladipeter <oladipeter@gmail.com>
2011-06-01 16:02:28 +0200

    Modify start controller

--- app/controllers/start_controller.rb
+++ app/controllers/start_controller.rb
@@ -3,6 +3,19 @@ class StartController < ApplicationController
   before_filter :authenticate_user!
 
   def index
+    # Nezzuk meg hogy ez e az elso belepese a felhasznalonak, ha igen akkor kuldjon egy email-t az adminisztratornak
+    # hogy, hogy allitsa be a jogosultsagait. Addig mig ez nem tortenik meg, a felhasznalo csak alap dolgokhoz
+    # ferhet hozza
+
+    # users table, sign_in_count = 1
+
+    @user = User.new
+    if current_user.sign_in_count == 1
+      @sign_in = "Mail to Admin"
+    else
+      @sign_in = "NOOO Mail to Admin"
+    end
+
   end
 
 end
--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,9 +1,10 @@
-ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => "gmail.com",
-  :user_name            => "oladipeter",
-  :password             => "va1Rooke",
-  :authentication       => "plain",
-  :enable_starttls_auto => true
-}
+#ActionMailer::Base.smtp_settings = {
+#  :address              => "smtp.gmail.com",
+#  :port                 => 587,
+#  :domain               => "gmail.com",
+#  :user_name            => "oladipeter",
+#  :password             => "va1Rooke",
+#  :authentication       => "plain",
+#  :enable_starttls_auto => true
+#}
+# /config/environments/development alatt van deklaralva
