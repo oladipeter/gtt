4931dac77933f2d9babcb4af92a3a3ba24e32ab6
oladipeter <oladipeter@gmail.com>
2011-08-18 09:22:46 +0200

    Fixed easyslider at user backend

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -3,6 +3,31 @@
 
 $(document).ready(function(){
 
+    $("#slider2").easySlider({
+				controlsBefore:	'<p id="controls2">',
+				controlsAfter:	'</p>',
+				prevId: 'prevBtn2',
+				nextId: 'nextBtn2',
+
+        prevText: 'Previous',
+        nextId: 'nextBtn',
+        nextText: 'Next',
+        controlsShow: true,
+        controlsFade: true,
+        firstId: 'firstBtn',
+        firstText: 'First',
+        firstShow: false,
+        lastId: 'lastBtn',
+        lastText: 'Last',
+        lastShow: false,
+        vertical: false,
+        speed: 800,
+        auto: true,
+        pause: 7000,
+        continuous: true
+
+	});
+
   $("#mikrovoks_tip").tipTip({
     defaultPosition: 'bottom',
     delay: 200
