62faab052a9864455d8fb06a30cb5be5b409a8c4
oladipeter <oladipeter@gmail.com>
2011-08-23 12:55:01 +0200

    Added image uplode to articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,4 @@
 class Article < ActiveRecord::Base
   belongs_to :system
+  has_attached_file :image, :styles => { :medium => "320x240>", :thumb => "80x60>" }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110818095408) do
+ActiveRecord::Schema.define(:version => 20110823080005) do
 
   create_table "aboutus", :force => true do |t|
     t.string   "title"
@@ -67,6 +67,10 @@ ActiveRecord::Schema.define(:version => 20110818095408) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "position"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "ckeditor_assets", :force => true do |t|
