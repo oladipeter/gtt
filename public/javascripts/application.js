43c98d2b12cc8d517b97bc56864a41857e8a7216
oladipeter <oladipeter@gmail.com>
2011-08-24 12:23:38 +0200

    Remove jquery slider

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -58,20 +58,4 @@ $(document).ready(function(){
     delay: 200
   });
 
-  /* SLIDER */
-
-  $(".slidetabs").tabs(".images > div", {
-
-        // enable "cross-fading" effect
-        effect: 'fade',
-        fadeOutSpeed: "slow",
-
-        // start from the beginning after the last tab
-        rotate: true
-
-        // use the slideshow plugin. It accepts its own configuration
-    }).slideshow();
-
-    $(".slidetabs").data("slideshow").play();
-
 });
