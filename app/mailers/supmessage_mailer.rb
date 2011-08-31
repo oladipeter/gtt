4cb84c14b46306c3072e27b31aa90d48d3f6d4e8
oladipeter <oladipeter@gmail.com>
2011-08-31 11:33:05 +0200

    Modified supmessage mailer and form

--- app/mailers/supmessage_mailer.rb
+++ app/mailers/supmessage_mailer.rb
@@ -7,8 +7,11 @@ class SupmessageMailer < ActionMailer::Base
     @supmessage = supmessage
     @url  = "http://#{default_url_options[:host]}/support_messages/datasheet/#{@supmessage.id}"
 
-    @supmessage_admins = @supmessage.admins
-      mail(:to => @supmessage_admins.all.map(&:email),
+    @system_title = @supmessage.system_title
+    @working_system = System.find(:first, :conditions => ['title = ?',@system_title])
+    @admins = @working_system.admins
+
+      mail(:to => @admins.all.map(&:email),
            :subject => "Globomax Kft. | Terméktámogatás oldal | #{@supmessage.system_title} hibabejelentés érkezett!")
 
   end
