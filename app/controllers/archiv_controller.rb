c81ccf0c88865444307907b0c94e933966f8d7a1
oladipeter <oladipeter@gmail.com>
2011-11-07 11:58:46 +0100

    order pos1

--- app/controllers/archiv_controller.rb
+++ app/controllers/archiv_controller.rb
@@ -3,7 +3,7 @@ class ArchivController < ApplicationController
   layout "globomax"
 
   def index
-    @pos1_articles = Article.find(:all, :conditions => { :position => ["1"] })
+    @pos1_articles = Article.find(:all, :conditions => { :position => ["1"] }, :order => "created_at DESC")
     @articles = Article.find(:all, :conditions => { :position => ["5"] })
   end
 
