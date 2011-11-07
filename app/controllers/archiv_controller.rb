e18bde6f2250a7d66bbb8339a64067f8912a7122
oladipeter <oladipeter@gmail.com>
2011-11-07 11:56:33 +0100

    Mod pos1 article at archivum

--- app/controllers/archiv_controller.rb
+++ app/controllers/archiv_controller.rb
@@ -3,6 +3,7 @@ class ArchivController < ApplicationController
   layout "globomax"
 
   def index
+    @pos1_articles = Article.find(:all, :conditions => { :position => ["1"] })
     @articles = Article.find(:all, :conditions => { :position => ["5"] })
   end
 
